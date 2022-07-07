509b27a954786d13462374f0d8e4c3a83c687db4
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-07-07 19:21:34 +0700

    Инициализация проекта

---
+++ src/test/java/kosterror/shift/ShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package kosterror.shift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
