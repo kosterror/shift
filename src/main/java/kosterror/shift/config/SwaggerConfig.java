ae7c68794febd625172fc5fde69807c84cf69ea3
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-07-13 21:47:26 +0700

    Пока что не починил поломку, но теперь запускается

--- src/main/java/kosterror/shift/config/SwaggerConfig.java
+++ src/main/java/kosterror/shift/config/SwaggerConfig.java
@@ -2,16 +2,14 @@ package kosterror.shift.config;
 
 import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
+//@Configuration
+//@EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
@@ -22,10 +20,4 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .paths(PathSelectors.any())
                 .build();
     }
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/swagger-ui/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
-//    }
 }
